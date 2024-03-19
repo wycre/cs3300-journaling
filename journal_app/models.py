3cde2c6f3e42ceac4fb9aa61b46bbc6534f9c3b9
wycre <djones@wycre.net>
2024-03-19 17:25:07 -0600

    Memo is now required

--- journal_app/models.py
+++ journal_app/models.py
@@ -7,6 +7,6 @@ class Journal(models.Model):
     """Handles User Journals, Users can have multiple Journals"""
     title = models.CharField(max_length=200, blank=False)
     author_name = models.CharField(max_length=200, blank=False)
-    memo = models.TextField(max_length=500)
+    memo = models.TextField(max_length=500, blank=False)
     is_public = models.BooleanField(default=False)
     journal_icon = models.ImageField(upload_to='uploads/journal_icons')
