efc41371161b4d7a5cd1ae9ab1306d89c84ff9ba
wycre <djones@wycre.net>
2024-03-19 17:14:58 -0600

    Journal Model

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'journal_app', 'static'),
 ]
 
-MEDIA_URL = 'images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'journal_app', 'media')
+MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
