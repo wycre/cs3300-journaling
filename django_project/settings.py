0881a786f4cd547c5f649ee2f2293464ab645fd5
Aclamendo <davidjones@ycre.net>
2024-04-21 12:15:01 -0600

    New integration test

--- django_project/settings.py
+++ django_project/settings.py
@@ -130,3 +130,5 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = "localhost:8000"
--- journal_app/tests/test_forms_journal_integration.py
+++ journal_app/tests/test_forms_journal_integration.py
@@ -1,6 +1,9 @@
 import os.path
+import random
 
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import TestCase, LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -10,17 +13,18 @@ from journal_app.models import Journal
 
 
 # Create your tests here.
-class JournalFormTest(TestCase):
+class JournalLoginTest(StaticLiveServerTestCase):
     """Test all forms related to Journals"""
 
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
         cls.driver = webdriver.Firefox()
 
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
-
+        super().tearDownClass()
 
     def test_form(self):
         # Initialize Test Values
@@ -28,8 +32,45 @@ class JournalFormTest(TestCase):
             os.path.join(os.path.dirname(__file__), "assets", "test_image.jpg")
         )
 
+        # Run Registration
+        # Make test account
+        self.driver.get('http://localhost:8000/profile/register')
+
+        # Get Form Fields
+        username = self.driver.find_element(By.XPATH, '//*[@id="id_username"]')
+        email = self.driver.find_element(By.XPATH, '//*[@id="id_email"]')
+        password1 = self.driver.find_element(By.XPATH, '//*[@id="id_password1"]')
+        password2 = self.driver.find_element(By.XPATH, '//*[@id="id_password2"]')
+        submit = self.driver.find_element(By.CSS_SELECTOR, '.container > form:nth-child(2) > input:nth-child(8)')
+
+        # Fill Form
+        rand_num = str(random.randint(1, 100))
+        username.send_keys("testuser" + rand_num)
+        email.send_keys("testemail" + rand_num + "@email.com")
+        password1.send_keys("testpass1")
+        password2.send_keys("testpass1")
+
+        print("User Number: " + rand_num)
+
+        # Submit form
+        submit.send_keys(Keys.RETURN)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(lambda condition: self.driver.current_url == 'http://localhost:8000/profile/login/')
+
+        # Log In
+        username = self.driver.find_element(By.XPATH, '//*[@id="id_username"]')
+        password1 = self.driver.find_element(By.XPATH, '//*[@id="id_password"]')
+        submit = self.driver.find_element(By.CSS_SELECTOR, '.container > form:nth-child(2) > input:nth-child(3)')
+
+        username.send_keys("testuser" + rand_num)
+        password1.send_keys("testpass1")
+
+        submit.send_keys(Keys.RETURN)
+        wait.until(lambda condition: self.driver.current_url == 'http://localhost:8000/')
+
         # Make a new Journal
         self.driver.get('http://localhost:8000/journal/new')
+        wait.until(lambda condition: self.driver.current_url == 'http://localhost:8000/journal/new')
 
         # Get Form Fields
         journal_title = self.driver.find_element(By.CSS_SELECTOR, '#title')
@@ -105,6 +146,14 @@ class JournalFormTest(TestCase):
         self.driver.find_element(By.XPATH, '/html/body/div/div/div/div/div/form/div/input').send_keys(Keys.ENTER)
 
         assert 'Test Journal Beta' not in self.driver.page_source
+
+
+        # Log out user
+        # Logout user
+        logout = self.driver.find_element(By.XPATH, '/html/body/div[1]/nav/div/div/div/div/a[4]')
+        logout.send_keys(Keys.RETURN)
+
+        #User.objects.get(username='testuser' + rand_num).delete()
 
 
 
